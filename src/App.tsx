db5dddd8a18eb62d74c3163db0ebc216dd3bbcc3
ByungJoon Lee <imjuni@gmail.com>
2025-10-10 10:51:52 +0900

    feat: add react-intl for internationalization support and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import '@xyflow/react/dist/style.css';
+import { Link as IconLink, Settings } from 'lucide-react';
+import { useIntl } from 'react-intl';
 
 import { Editor } from '#/components/editor/Editor';
 import { Nav } from '#/components/nav/Nav';
 import { XYFlowRenderer } from '#/components/renderer/xyflow/XYFlowRenderer';
+import { Button } from '#/components/ui/button';
 
 import './App.css';
 
-export const App = () => (
-  <div className="w-full h-full">
-    <Nav />
+export const App = () => {
+  const intl = useIntl();
 
-    <main className="mt-13 h-full">
-      <div className="flex flex-row h-full">
-        <div className="flex flex-3 h-full">
-          <div className="flex flex-col h-full w-full">
-            <div className="h-10 px-4 bg-card flex justify-end"> </div>
-            <div className="h-full">
-              <Editor />
+  return (
+    <div className="w-full h-full">
+      <Nav />
+
+      <main className="mt-13 h-full">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-3 h-full">
+            <div className="flex flex-col h-full w-full">
+              <div className="h-10 px-4 bg-card flex justify-end items-center space-x-1">
+                <Button size="sm" variant="outline">
+                  {intl.$t({ id: 'import' })}
+                </Button>
+
+                <Button size="sm" variant="outline">
+                  <IconLink />
+                </Button>
+
+                <Button size="sm" variant="outline">
+                  <Settings />
+                </Button>
+              </div>
+              <div className="h-full">
+                <Editor />
+              </div>
             </div>
           </div>
+          <div className="flex flex-7 h-full">
+            <XYFlowRenderer />
+          </div>
         </div>
-        <div className="flex flex-7 h-full">
-          <XYFlowRenderer />
-        </div>
-      </div>
-    </main>
-  </div>
-);
+      </main>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
+import { IntlProvider } from 'react-intl';
 
 import { App } from './App';
 import './index.css';
 
+const messages = import.meta.glob('./i18n/*.json', { eager: true });
+const locale = localStorage.getItem('locale') ?? 'en';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const message = (messages[`./i18n/${locale}.json`] as any).default;
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <IntlProvider defaultLocale="en" locale={locale} messages={message as Record<string, string>}>
+      <App />
+    </IntlProvider>
   </StrictMode>,
 );
