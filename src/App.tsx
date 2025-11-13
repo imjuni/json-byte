afec38b595a63bc563dbdb2596bb8ad79a800d31
ByungJoon Lee <imjuni@gmail.com>
2025-11-14 02:41:15 +0900

    fix: replace PixiGraphRenderer with XYFlowRenderer in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import { FooterEditorStatus } from '#/components/layout/FooterEditorStatus';
 import { Resizer } from '#/components/layout/Resizer';
 import { Nav } from '#/components/nav/Nav';
 import { Notification } from '#/components/nav/Notification';
-import { PixiGraphRenderer } from '#/components/renderer/pixi/PixiGraphRenderer';
-// import { XYFlowRenderer } from '#/components/renderer/xyflow/XYFlowRenderer';
+import { XYFlowRenderer } from '#/components/renderer/xyflow/XYFlowRenderer';
 import { useAppStore } from '#/stores/appStore';
 
 import './App.css';
@@ -39,7 +38,7 @@ export const App = () => {
             id="graph-tree-control-container"
             style={{ width: `${100 - editorWidthPercent}%` }}
           >
-            <PixiGraphRenderer />
+            <XYFlowRenderer />
           </div>
         </BrowserView>
 
@@ -61,7 +60,7 @@ export const App = () => {
             id="graph-tree-control-container"
             style={{ height: `${100 - editorHeightPercent}%` }}
           >
-            <PixiGraphRenderer />
+            <XYFlowRenderer />
           </div>
         </MobileView>
       </main>
